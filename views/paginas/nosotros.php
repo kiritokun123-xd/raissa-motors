8b50c0da892f559565cd9655b3ff12c18577234e
unknown <david22android@gmail.com>
2022-06-10 17:53:11 -0500

    nosotros

--- views/paginas/nosotros.php
+++ views/paginas/nosotros.php
@@ -19,9 +19,9 @@
             <p>RaissaMotors inicio en el año 2003 con venta de repuestos para vehiculos menores en el distrito del Rimac, años después nos mudamos a la Av 22 de agosto en el Mercado Modelo, iniciando con la fabricación de Mototaxis.</p>
         </div>
         <div class="info">
-            <p>Somos una Empresa con más de 18 años en el Mercado Nacional, dedicada a la fabricación de Mototaxis, Cargueros, Motocicletas. Contamos con personal altamente calificado con autorización del Ministerio de Producción.</p>
-            <p>Con sucursal en Barranca, Puente Piedra, Comas y con Distribuidores a nivel Nacional como Chimbote, Trujillo, Casma, Barranca. </p>
-            <p>RaissaMotors inicio en el año 2003 con venta de repuestos para vehiculos menores en el distrito del Rimac, años después nos mudamos a la Av 22 de agosto en el Mercado Modelo, iniciando con la fabricación de Mototaxis.</p>
+            <p>Con el tiempo fuimos mejorando nuestrás técnicas en Ensamblaje, Pintura, Soldadura, y escogiendo los mejores productos de calidad, como los aros, los tubos, los motores, etc.</p>
+            <p>Logramos Patentar nuestra propia Marca y los más importante tener nuestra propia Serie (VIN) lo cual indica que estamos siendo supervisador por el Ministerio de Prodcción, eso te garantiza un producto de mejor calidad.</p>
+            <p>Esperemos que tengas una grata visita a nuestras tiendas o fábricas, y recuerda... Más que elegancia somos Calidad!</p>
         </div>
         <img src="../imagenes/TIENDA.jpg" alt="">
     </div>
@@ -29,11 +29,14 @@
 
     <div class="mvo">
         <h3>Misión</h3>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Non nisi tenetur consectetur numquam repellat? Blanditiis, minima quisquam magnam maxime dicta excepturi autem quidem officia perferendis ratione inventore veritatis ad facilis.</p>
+        <p>Liderar la industria de vehículos menores   con la innovación de nuevos diseños que satisfaga las necesidades de nuestros clientes y distribuidores . Donde además les ofreceremos variedad de marcas basadas en la alta calidad y durabilidad del producto.
+Somos fabricantes y trabajamos para brindarles un excelente servicio.</p>
         <h3>Visión</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut animi totam dicta consectetur sequi architecto repudiandae et. Quibusdam error, accusamus aliquid quis molestiae iusto veritatis ab corrupti pariatur repellat officiis.</p>
-        <h3>Objetivo</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia velit aliquid ab quam dolore excepturi, blanditiis perferendis perspiciatis vitae corporis ullam. Optio quia magni animi odio alias quisquam dolor nisi!</p>
+        <p>La mejor marca a nivel nacional y mundial , siendo reconocidos por la calidad estructural y por los productos originales .  Con Raissa Motor's siente la confianza y seguridad de llegar a tu destino.</p>
+        <h3>Valores </h3>
+        <p> <b>Honestidad</b> .- Consideramos que lo que necesita la humanidad es que hablemos y actuemos con sinceridad.</p>
+        <p> <b>Respeto</b> .- Es uno de los valores pilares en nuestra empresa para poder trabajar en equipo , ya que la unión hace la fuerza.</p>
+        <p> <b>Gratitud</b> .- Estamos agradecidos con nuestros clientes por la confianza y valoramos todo el esfuerzo que hacen para adquirir su vehículo.</p>
     </div>
 </div>
 
